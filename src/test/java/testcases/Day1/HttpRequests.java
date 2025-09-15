58f46291a77e7e2b129def7dc71b0c823a0a7ea6
Ankit Yadav <ankiii0071@gmail.com>
2025-09-16 00:22:47 +0530

    create,update,get,delete - Http requests check day 1

--- src/test/java/testcases/Day1/HttpRequests.java
+++ src/test/java/testcases/Day1/HttpRequests.java
@@ -11,7 +11,8 @@ import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.ResponseAwareMatcher.*;
 import static org.hamcrest.Matchers.*;
 
-public class HTTPRequests {
+public class HttpRequests {
+
 
     private int productId;
 
